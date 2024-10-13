865a43e790f21e02207d72caba590303e7a2ef96
Alessandra Ribeiro Guimaraes <alessandra1ribeiroguimaraes@gmail.com>
2024-10-13 17:50:26 -0300

    refactor: templates noticias_table e search; urls e views

--- app_j24/views.py
+++ app_j24/views.py
@@ -27,9 +27,11 @@ class NoticiaBase(PermissionRequiredMixin):
 
     def get_success_url(self):
         if self.request.user.has_perm('app_j24.noticia_criar'):
-            return reverse('noticias_autor')
+            # return reverse('noticias_autor')
+            return reverse('noticias')
         if self.request.user.has_perm('app_j24.pode_publicar'):
-            return reverse('editor_noticias')
+            # return reverse('editor_noticias')
+            return reverse('noticias')
         return reverse('home')
 
 class NoticiaCadastro(NoticiaBase):
@@ -64,6 +66,61 @@ class NoticiaDelete(NoticiaBase, DeleteView):
     permission_required = "app_j24.noticia_excluir"
     template_name = 'noticia_confirm_delete.html'
 
+class NoticiasListView(ListView):
+    '''
+    Listar as nóticias dos autores e editores
+    '''
+    titulo = ''
+    categoria = 0     
+    model = Noticia
+    template_name = 'noticias_table.html'
+
+    def post(self, request, *args, **kwargs):
+        self.titulo = ''
+        self.categoria = 0   
+        if self.request.method == 'POST':
+            self.titulo = self.request.POST['titulo']
+            if self.request.POST['categoria']:
+                self.categoria = int(self.request.POST['categoria'])
+
+        return render(request, 'noticias_table.html', {
+            "titulo": self.titulo,
+            "categoria": self.categoria,
+            "categorias": Categoria.objects.all(),
+            "object_list": self.get_queryset(),
+        })
+
+    def get_queryset(self):
+        filtragem = {}
+        if self.titulo:
+            filtragem['titulo__icontains'] = self.titulo
+        if self.categoria != 0:
+            filtragem['categoria'] = self.categoria
+        if not self.request.user.has_perm('app_j24.pode_publicar'):
+            filtragem['autor'] = self.request.user
+        
+        if filtragem:
+            return Noticia.objects.filter(**filtragem)
+
+        return Noticia.objects.all()
+
+        # if self.titulo and self.categoria != 0:
+        #     print('111111111111111111111111')
+        #     return Noticia.objects.filter(titulo__icontains=self.titulo, categoria=self.categoria)
+        # if self.titulo:
+        #     print('2222222222222222222222222')
+        #     return Noticia.objects.filter(titulo__icontains=self.titulo)
+        # if self.categoria != 0:
+        #     print('3333333333333333333333333333333')
+        #     return Noticia.objects.filter(categoria=self.categoria)          
+        # print('xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx')
+        # return Noticia.objects.all()
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['categorias'] = Categoria.objects.all()     
+        return context
+
 
 class HomeListView(ListView):
     '''
@@ -74,9 +131,6 @@ class HomeListView(ListView):
     model = Noticia
     template_name = 'home.html'
 
-    def get(self, request, *args, **kwargs):
-        return super().get(request, *args, **kwargs)
-
     def post(self, request, *args, **kwargs):
         self.titulo = ''
         self.categoria = 0   
@@ -84,11 +138,6 @@ class HomeListView(ListView):
             self.titulo = self.request.POST['titulo']
             if self.request.POST['categoria']:
                 self.categoria = int(self.request.POST['categoria'])
-
-        print('______________________________________________')
-        print(f"titulo=|{self.titulo}|")
-        print(f"catergoria=|{self.categoria}| TIPO={type(self.categoria)}")
-        print('______________________________________________')
 
         return render(request, 'home.html', {
             "titulo": self.titulo,
@@ -110,9 +159,6 @@ class HomeListView(ListView):
         return Noticia.objects.filter(publicada=True).order_by('-publicada_em')
 
     def get_context_data(self, **kwargs):
-        print('kkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk')
-        print(f"kwargs={kwargs}")
-        print('ccccccccccccccccccccccccccccccccccccccccccc')
         context = super().get_context_data(**kwargs)
         context['categorias'] = Categoria.objects.all()     
         return context
@@ -125,6 +171,7 @@ class NoticiaDetailView(DetailView):
     '''
     model = Noticia
     template_name = 'noticia.html'
+
 
 @login_required(login_url="/accounts/login/")
 @permission_required("app_j24.noticia_criar")
@@ -143,7 +190,6 @@ def noticias_autor(request) -> HttpResponse:
     form = PesquisarNoticiaForm()
     return render(request, 'noticias_table.html', {
         "form": form,
-        # "noticias_autor": lista,
         "noticias": lista,
     })
 
@@ -157,7 +203,6 @@ def editor_noticias(request) -> HttpResponse:
     form = PesquisarNoticiaForm()
     return render(request, 'noticias_table.html', {
         "form": form,
-        # "editor_noticias": lista,
         "noticias": lista,
     })
 
@@ -194,9 +239,6 @@ def publicar_noticia(request, noticia_id: int, publicado: int) -> HttpResponse:
     '''
     Publicar Notícia
     '''
-    print('publicar_noticia.............................')
-    print(f"noticia_id={noticia_id}")
-    print(f"publicado={publicado}")
     try:
         noticia = Noticia.objects.get(pk=noticia_id)
         if publicado == 1:
@@ -210,4 +252,4 @@ def publicar_noticia(request, noticia_id: int, publicado: int) -> HttpResponse:
         raise Http404(
             f"Notícia não encontrada! Não foi possível publicar a Notícia de ID={noticia_id} porque ela não existe na base de dados."
         ) from not_found
-    return HttpResponseRedirect(reverse("editor_noticias"))
+    return HttpResponseRedirect(reverse("noticias"))
